28a9d3d38d2ad3c8b852343c369c96ea29557132
yanghuangkun <347774946@qq.com>
2020-06-23 14:53:38 +0800

    今生第一次提交代码

---
+++ src/main/java/com/igeek/springBootHollow.java
@@ -0,0 +1,11 @@
+package com.igeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springBootHollow {
+    public static void main(String[] args) {
+        SpringApplication.run(springBootHollow.class,args);
+    }
+}
